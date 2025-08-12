fe1d312327c135a50a291b8d42b79badcf049aee
unknown <admin@o-kurbanov-f.ent-en.com>
2025-08-12 16:13:08 +0500

    Консультации 1

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,11 +4,14 @@ from .order_service import OrderService
 from .notification_service import NotificationService
 from .florist_service import FloristService
 from .consultation_service import ConsultationService
+from .ai_archive_service import AIArchiveService
+
 __all__ = [
     "UserService",
     "CatalogService", 
     "OrderService",
     "NotificationService",
     "FloristService",
-    "ConsultationService"
+    "ConsultationService",
+    "AIArchiveService"
 ]
